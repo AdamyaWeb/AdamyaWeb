ae628991950d389d71ac9e8c0521462c5e70a8b5
Andy <105436399+AdamyaWeb@users.noreply.github.com>
2022-06-24 11:55:46 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+num1 = input("Enter a number: ");num2 = input("Enter the second number: ")
+oprtr = input("Enter the Operator: ")
+
+if oprtr == "+":
+    reslt = float(num1) +  float(num2)
+    print(reslt)
+if oprtr == "-":
+    reslt = float(num1) -  float(num2)
+    print(reslt)
+if oprtr == "*":
+    reslt = float(num1) *  float(num2)
+    print(reslt)
+if oprtr == "/":
+    reslt = float(num1) /  float(num2)
+    print(reslt)
+if oprtr == "**":
+    reslt = float(num1) ** float(num2)
+    print(reslt)
+
+
+
+
